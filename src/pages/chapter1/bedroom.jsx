7594bd256c303173b51666da83bdd51b1a5f6997
rromero123102 <rromero21332@columbushs.com>
2021-02-14 22:02:34 -0500

    chapter 1 part 2 complete

--- src/pages/chapter1/bedroom.jsx
+++ src/pages/chapter1/bedroom.jsx
@@ -3,16 +3,33 @@ import { Link } from 'react-router-dom';
 import CharacterLeft from '../../components/CharacterLeft';
 import CharacterRight from '../../components/CharacterRight';
 
-const dialogue = ["COME ON, COME ON","HEYAAAAA!","WOAH WHAT THE HECK MAN!","noooooo"]
+const dialogue = ["COME ON, COME ON", "Don't worry BMO I'm going to w---","HEYAAAAA!","WOAH WHAT THE HECK MAN!","If I wasn't strechy I would have totaly been killed!","And Who's this clown, Sup Buddy.", "This is a stranger who I asked for help in getting you off BMO.", "Dude, you shouldn't be letting random people in our hous-- OH MY GOD BMO!", "ARGGH I was about to win before you clowns ruined everything", "Why are you still playing that game anyway its been like 3 weeks?", "BMO got corrupted and I have to win this game in order to get him back.", "Im starting to lose hope because I can't make any progress on this dumb game.", "Hey stranger, why don't you give it a try, maybe you will be sorta useful"]
 
 const Bedroom=()=>{
+    const [visible, setVisible] = useState(false);
     const [lineCount, setLineCount] = useState(0)
     const zindex={
-        zIndex: 3
+        zIndex: 11
     }
     const changeLevel={
         zIndex: lineCount
     }
+    const attack={
+        display: visible ? "block":"none"
+    }
+    //!find a better way to do this\/\/\/\/
+    useEffect(()=>{
+        if(lineCount == 2){
+            setVisible(true)
+        }
+        else if(lineCount == 3){
+            setVisible(false)
+        }
+        else if(lineCount == 6){
+            setVisible(true)
+        }
+    }, [lineCount])
+    //!find a better way to do this^^^
     function nextLine(){
 
         setLineCount(lineCount+1)
@@ -26,6 +43,14 @@ const Bedroom=()=>{
                     characterI='jake'
                     Text={dialogue[lineCount]}
                 />
+                <div style={attack}>
+                    <CharacterRight
+                        characterI='finn'
+                        Text={dialogue[lineCount]}
+                    />
+                </div>
+                   
+
             </div>
             
         </div>
